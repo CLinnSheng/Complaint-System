da57173dfdff1823a8428693be4c569717e78f35
linnsheng <135851909+CLinnSheng@users.noreply.github.com>
2024-08-21 21:53:02 +0800

    Update complaint.h

--- complaint.h
+++ complaint.h
@@ -3,55 +3,55 @@
 #include <iostream>
 using namespace std;
 
-class Complaint{
+class Complaint {
 
     protected:
-        string description;
+	string description;
         string category;
         string status;
 
     public:
         Complaint(string desc, string cat, string stat) : description(desc), status(stat), category(cat) {}
-        void setDescription(string desc){
+        void setDescription(string desc) {
             description = desc;
         }
-        void setStatus(string stat){
+        void setStatus(string stat) {
         	status = stat;
-		}
-        string getDescription() const{
-            return description;
+	}
+        string getDescription() const {
+        	return description;
         }
-        string getStatus() const{
+        string getStatus() const {
         	return status;
-		}
-		string getCategory() const{
-			return category;
-		}
+	}
+	string getCategory() const {
+		return category;
+	}
 
 };
 
-class Arcade : public Complaint{
+class Arcade : public Complaint {
     
     public:
         Arcade(string desc, string cat, string status = "Pending") : Complaint(desc, cat, status) {}
 
 };
 
-class Faculty : public Complaint{
+class Faculty : public Complaint {
 
     public:
         Faculty(string desc, string cat, string status = "Pending") : Complaint(desc, cat, status) {}
 
 };
 
-class Hostel : public Complaint{
+class Hostel : public Complaint {
 
     public:
         Hostel(string desc, string cat, string status = "Pending") : Complaint(desc, cat, status) {}
 
 };
 
-class Other : public Complaint{
+class Other : public Complaint {
 
     public:
         Other(string desc, string cat, string status = "Pending") : Complaint(desc, cat, status) {}
